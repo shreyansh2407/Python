f2c6add83f8fcffcd5fb712986533d6f51e3a8a3
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-06 22:20:50 +0530

    variables in python

---
+++ variables.py
@@ -0,0 +1,27 @@
+
+
+
+#variables in python
+
+
+a=5 #integer
+print(a)
+
+b=7
+print(b)
+
+a=6.2 #floating point number
+print(a)
+
+c=True  #boolean 
+print(c)
+
+d=False
+print(d)
+
+e="Shreyansh"  # string
+print(e)
+
+e= None # none
+print(e)
+
