52d9165d5a44fde26e6d1b69132def0b43c1ea6d
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-10 01:19:18 +0530

    Input

---
+++ input.py
@@ -0,0 +1,14 @@
+name= input("Enter your name:")
+
+print(name)
+
+#Write a python program to enter number as input and add 6 to it
+
+num = int(input("Enter your number:"))
+print(num+6)
+
+
+a1="Shreyansh"
+a2="kumar"
+a3="singh"
+print(a1,a2,a3)
