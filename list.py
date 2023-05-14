384be984e2a3ce258a6c26e97b7f96434abe00c3
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-15 00:17:40 +0530

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+li=[1,2,3,55,34,5,7,"Shreyansh"]
+print(type(li))
+print(li)
+
+#list methods
+
+li.remove("Shreyansh")
+# li.sort()
+li.pop()
+li.append(45)
+li.clear()
+li.extend([1,4,2])
+print(li)
