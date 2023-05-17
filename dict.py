11b5264603d09ec4b0960c2e62b91af9f72bd1dd
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-05-17 21:50:40 +0530

    Dictionary

---
+++ dict.py
@@ -0,0 +1,12 @@
+a ={} # empty dictionary
+
+b= set() #empty set you cannot declare empty set by upper technique
+print(type(a))
+print(type(b))
+
+marks = {"Shreyansh":99,"Yuvraj":78,"Kuldeep":89}
+print(marks)
+print(marks["Shreyansh"]) #dictionary is mutable means we can modify it
+marks["Priyanka"] = 56
+print(marks)
+print(marks.items())
