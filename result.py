ec1c7d381f6c4318898149dd506dbe65db75757e
Shreyansh kumar <113797784+shreyansh2407@users.noreply.github.com>
2023-06-07 08:39:19 +0530

    Result of three subjects

---
+++ result.py
@@ -0,0 +1,7 @@
+physics  = int(input("Enter your physics marks:"))
+chemistry  = int(input("Enter your chemistry marks:"))
+mathematics  = int(input("Enter your mathematics marks:"))
+total = physics+chemistry+mathematics
+percentage = total/300*100
+print("Total marks is:",total)
+print("Percentage is:",percentage)
